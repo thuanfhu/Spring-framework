2a2a682b91bdae7c5b035d9143e1050bd16e74ad
Quách Phú Thuận <thuanmobile1111@gmail.com>
2024-12-14 15:02:57 +0700

    Day 8.1: Config spring security permit or authenticated endpoints and disable csrf

---
+++ src/main/java/vn/thuanflu/identityservices/configuration/SecurityConfig.java
@@ -0,0 +1,26 @@
+package vn.thuanflu.identityservices.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final String[] PUBLIC_ENDPOINTS = {"/users", "/auth/introspect", "/auth/token"};
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(request -> request
+                .requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
+                .anyRequest().authenticated());
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        return httpSecurity.build();
+    }
+
+}
